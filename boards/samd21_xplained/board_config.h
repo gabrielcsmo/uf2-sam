73d227b63eb3a4bc77e90c888c2ff923a201e0b2
Cosmin-Gabriel Samoila <gabrielcsmo@gmail.com>
2024-01-01 21:46:07 +0200

    Add SAMD21 Xplained PRO board
    Signed-off-by: Cosmin-Gabriel Samoila <gabrielcsmo@gmail.com>

---
+++ boards/samd21_xplained/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Microchip"
+#define PRODUCT_NAME "D21Xplained PRO"
+#define VOLUME_LABEL "D21-BOOT"
+#define INDEX_URL "http://www.microchip.com/"
+#define BOARD_ID "SAMD21-Xplained-PRO"
+
+#define USB_VID 0x239A
+#define USB_PID 0x0015
+
+#define LED_PIN PIN_PA17
+
+#endif
